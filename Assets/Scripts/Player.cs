6dde2fe93259838b3c7978b25d22dadd719a55f3
oam114 <oam8118@nyu.edu>
2022-10-02 12:40:15 -0400

    Bullets and Bombs
    initial attacks: they don't damage anything yet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,12 @@ public class Player : MonoBehaviour
     NavMeshAgent _agent;
     Camera main_cam;
 
+    public GameObject bulletPrefab;
+    int bulletForce = 500;
+
+    public GameObject bombPrefab;
+    int bombThrow = 750;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +30,19 @@ public class Player : MonoBehaviour
             if (Physics.Raycast(main_cam.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
                 _agent.SetDestination(hit.point);
             }
+        }
+        else if(Input.GetMouseButtonDown(1)) {
+            RaycastHit hit;
+            if (Physics.Raycast(main_cam.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                transform.LookAt(hit.point);
+                GameObject newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+                newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+            }
+
+        }
+        else if(Input.GetKeyDown("space")) {
+            GameObject newBomb = Instantiate(bombPrefab, transform.position, transform.rotation);
+            newBomb.GetComponent<Rigidbody>().AddForce(transform.forward * bombThrow);
         }
     }
 }
