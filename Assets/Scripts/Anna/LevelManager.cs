9b6c194ea9d4a0e5cc558f1720168b54fc087640
tc2965 <tc2965@nyu.edu>
2022-10-04 21:39:16 -0400

    add healthbar to the other scenes

---
+++ Assets/Scripts/Anna/LevelManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public int num_keys;
+    bool[] keys_collected;
+
+    int curr_keys = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys_collected = new bool[num_keys]; // will all be intialized to default bool (false)
+    }
+
+    public void addKey() {
+        keys_collected[curr_keys++] = true;
+        print(curr_keys);
+    }
+
+    public bool accessDoor(int door_code) {
+        return keys_collected[door_code];
+    }
+
+    public void ResetLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
